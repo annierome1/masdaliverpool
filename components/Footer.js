944cdefacc4694f3f66611bd79dbaf26830c3fb2
Annie Rome <anniecrome1@gmail.com>
2025-05-19 16:41:10 -0400

    updates

--- components/Footer.js
+++ components/Footer.js
@@ -1,47 +1,52 @@
-import styles from '../styles/components/footer.module.css';
-import {
-  FaInstagram,
-  FaFacebookF,
-  FaYoutube,
-} from 'react-icons/fa';
-import Image from 'next/image';
+// components/Footer.jsx
+import styles from '../styles/components/footer.module.css'
+import { FaInstagram, FaFacebookF, FaYoutube } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <div>
-          <Image src="/masda_logo.png" alt="Logo" width={180} height={80} />
-          <p className="mt-4 text-gray-400">Train Hard. Fight Smart. Elevate.</p>
-        </div>
+        {/* Quick Links */}
         <div className={styles.column}>
           <h3>Quick Links</h3>
           <ul className={styles.linkList}>
-            <li><a href="#">Classes</a></li>
-           {/* <li><a href="#">Events</a></li>*/}
-            <li><a href="#">Pricing</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="/team">Team</a></li>
+            <li><a href="/classes">Classes</a></li>
+            <li><a href="/sponsors">Sponsors</a></li>
+             <li><a href="/contact">Contact Us</a></li>
           </ul>
         </div>
+
+        {/* Logo & Tagline */}
+        <div className={`${styles.column} ${styles.logo}`}>
+          <Image src="/masda_logo.png" alt="Logo" width={180} height={80} />
+          <p className={styles.tagline}>
+            Train Hard. Fight Smart. Elevate.
+          </p>
+        </div>
+
+        {/* Social Icons */}
         <div className={styles.column}>
           <h3>Follow Us</h3>
           <div className={styles.socialIcons}>
-            <a href="https://www.instagram.com/masdagym/?hl=en" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
-            </a>
-            <a href="https://www.facebook.com/masdagym/" target="_blank" rel="noopener noreferrer">
-                <FaFacebookF />
-            </a>
-            <a href="https://www.youtube.com/c/MasdaGymLiverpool" target="_blank" rel="noopener noreferrer">
-                <FaYoutube />
-            </a>
-            </div>
-
+            <Link href="https://www.instagram.com/masdagym/?hl=en" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </Link>
+            <Link href="https://www.facebook.com/masdagym/" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF />
+            </Link>
+            <Link href="https://www.youtube.com/c/MasdaGymLiverpool" target="_blank" rel="noopener noreferrer">
+              <FaYoutube />
+            </Link>
+          </div>
         </div>
       </div>
+
       <div className={styles.footerBottom}>
         © 2025 Masda Liverpool. All rights reserved.
       </div>
     </footer>
-  );
+  )
 }
